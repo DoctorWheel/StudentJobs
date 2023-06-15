e63b80c6ac608f3fe83f5cfe630dc5bfff3a4b1a
DoctorWheel <119934014+DoctorWheel@users.noreply.github.com>
2023-06-15 22:34:21 +0200

    Added templates.js + Updated about.php

--- about.php
+++ about.php
@@ -1,2 +1,12 @@
-<!doctype html>
-About
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="templates.js"></script>
+</head>
+<body>
+    <header-template></header-template>
+</body>
+</html>
