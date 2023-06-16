01d4b3b0e10f6b273b22fa09bed0eed6f18b68c3
DoctorWheel <119934014+DoctorWheel@users.noreply.github.com>
2023-06-16 13:17:59 +0200

    Added header

--- contact.php
+++ contact.php
@@ -1,2 +1,13 @@
-<!doctype html>
-Contact
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="templates.js"></script>
+</head>
+<body>
+    <header-template></header-template>
+    Contact
+</body>
+</html>
