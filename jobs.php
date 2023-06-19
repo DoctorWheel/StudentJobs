90c0ee66009d346eb6385b569035e14b5b1a3b3e
DoctorWheel <119934014+DoctorWheel@users.noreply.github.com>
2023-06-19 17:53:06 +0200

    Update jobs.php, partners.php

--- jobs.php
+++ jobs.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="templates.js"></script>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <div class="page">
         <header-template></header-template>
         Jobs
-        footer-template
+        <footer-template></footer-template>
     </div>
 </body>
 </html>
