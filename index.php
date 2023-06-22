57f7a966101e71c27549935c3608ce200ed6d4b0
DoctorWheel <119934014+DoctorWheel@users.noreply.github.com>
2023-06-22 16:21:41 +0200

    Change Header

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if (isset($_SESSION["user_type"])) {
     <div class="header">
       <div class="name" onclick="location.href='index.php';" style="cursor: pointer;"><h1>StudentJobs</h1><img src="images/icon.png" alt="Logo" style="width: 2.5vw; margin-left: 0.4vw;"></div>
       <div class="navigation">
-          <span class="nav-item active">HOME</span>
+          <span class="nav-item" onclick="location.href='index.php';" style="cursor: pointer;">HOME</span>
           <span class="nav-item" onclick="location.href='jobs.php';" style="cursor: pointer;">JOBS</span>
           <span class="nav-item" onclick="location.href='partners.php';" style="cursor: pointer;">PARTNERS</span>
           <span class="nav-item" onclick="location.href='about.php';" style="cursor: pointer;">ABOUT</span>
